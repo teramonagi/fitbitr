bdd250172f6da0af2919f941b02390c5f54d3755
Nagi Teramo <teramonagi@gmail.com>
2018-01-03 12:48:42 +0900

    update

--- R/activity.R
+++ R/activity.R
@@ -2,6 +2,14 @@
 # Constants
 url_activity <- paste0(url_api, "activities/")
 
+make_get_function <- function(url)
+{
+  function(token, simplify=TRUE)
+  {
+    tidy_output(get(url, token), simplify)
+  }
+}
+
 #' @title Get Daily Activity Summary
 #'
 #' @description
@@ -9,18 +17,17 @@ url_activity <- paste0(url_api, "activities/")
 #'
 #' @inheritParams inheritparams_token
 #' @inheritParams inheritparams_date
-#'
-#' @return A list
+#' @inheritParams inheritparams_simplify
 #'
 #' @details
 #' See \url{https://dev.fitbit.com/reference/web-api/activity/#get-daily-activity-summary} for more details.
 #'
 #' @export
-get_activity_summary <- function(token, date)
+get_activity_summary <- function(token, date, simplify=TRUE)
 {
   url <- paste0(url_activity, sprintf("date/%s.json", format_date(date)))
-  response <- get(url, token)
-  convert_content_to_r_object(response)
+  # We can not simplify this output because it is so complicated nested list
+  tidy_output(get(url, token), simplify=FALSE)
 }
 
 #' @title Get Activity Time Series
@@ -34,7 +41,7 @@ get_activity_summary <- function(token, date)
 #' @param end_date The end date of the range. A Date class object or a string in the format yyyy-MM-dd.
 #' @param date The end date of the period specified. A Date class object or a string in the format yyyy-MM-dd.
 #' @param period The range for which data will be returned. Options are "1d", "7d", "30d", "1w", "1m", "3m", "6m", "1y", or "max".
-#' @param simplify coerce a list into a data.frame
+#' @inheritParams inheritparams_simplify
 #'
 #' @details
 #'  Available resource_path are
@@ -65,21 +72,6 @@ get_activity_time_series <- function(token, resource_path, date="", period="", b
   tidy_output(get(url, token), simplify)
 }
 
-tidy_output <- function(response, simplify)
-{
-  content <- convert_content_to_r_object(response)
-  if(!simplify){return(content)}
-  if(is.data.frame(content)){return(content)}
-  if(length(content) == 0){return(content)}
-
-  #Stop redundant warnings
-  old <- options(warn = -1)
-  result <- Reduce(cbind, lapply(content, as.data.frame))
-  options(old)
-  names(result) <- stringr::str_replace_all(names(result), "\\.", "_")
-  result
-}
-
 #' @title Get Activity Intraday Time Series
 #'
 #' @description
@@ -92,6 +84,7 @@ tidy_output <- function(response, simplify)
 #' @param detail_level Number of data points to include. Either 1min or 15min. Optional.
 #' @param start_time The start of the period, in the format HH:mm. Optional.
 #' @param end_time The end of the period, in the format HH:mm. Optional.
+#' @inheritParams inheritparams_simplify
 #'
 #' @details
 #'  Available resource_path are
@@ -122,66 +115,41 @@ get_activity_intraday_time_series <- function(token, resource_path, date, detail
   tidy_output(get(url, token), simplify)
 }
 
-
-#' @title Get Activity Logs List
-#'
-#' @descriptions
-#'   \code{get_activity_logs_list()} retrieves a list of a user's activity log entries before or after a given day with offset and limit
-#'
-#' @inheritParams inheritparams_token
-#' @param before_date The date in the format yyyy-MM-ddTHH:mm:ss. Only yyyy-MM-dd is required.
-#'   Either beforeDate or afterDate should be specified.
-#' @param after_date The date in the format yyyy-MM-ddTHH:mm:ss.
-#' @param sort The sort order of entries by date asc (ascending) or desc (descending).
-#' @param offset The offset number of entries.
-#' @param limit The max of the number of entries returned (maximum: 100)
-#'
-#' @details
-#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#get-activity-logs-list} for more details.
-#'
-#' @export
-get_activity_logs_list <- function(token, before_date, after_date, sort, offset, limit)
-{
-  stop("Sorry, under construction")
-  #url <- paste0(url_activity, "list.json")
-  #convert_content_to_r_object(get(url, token))
-}
-
 #' @title Get Activity Types
 #'
 #' @description
 #'   Get a tree of all valid Fitbit public activities from the activities catalog as well as private custom activities the user created
 #'
 #' @inheritParams inheritparams_token
+#' @inheritParams inheritparams_simplify
 #'
 #' @export
-get_activity_types <- function(token)
+get_activity_types <- function(token, simplify=TRUE)
 {
-  url <- paste0(url_base, "activities.json")
+  url <- paste0(url_activity, "activities.json")
+  # We can not simplify this output because it is so complicated nested list
   tidy_output(get(url, token), simplify=FALSE)
 }
 
-
-#' Get Activity Type
+#' @title Get Activity Type
 #'
-#' Returns the details of a specific activity in the Fitbit activities database in the format requested.
-#' If activity has levels, also returns a list of activity level details.
+#' @description
+#'   \code{get_activity_type()} returns the details of a specific activity in the Fitbit activities database.
 #'
 #' @inheritParams inheritparams_token
 #' @param activity_id The activity ID.
+#' @inheritParams inheritparams_simplify
 #'
 #' @export
-get_activity_type <- function(token, activity_id)
+get_activity_type <- function(token, activity_id, simplify=TRUE)
 {
-  #GET https://api.fitbit.com/1/activities/[activity-id].json
   url <- paste0(url_base, sprintf("activities/%s.json", activity_id))
-  response <- get(url, token)
-  convert_content_to_r_object(response)
+  tidy_output(get(url, token), simplify)
 }
 
 #' @title Get Frequent Activities
 #'
-#' @descriptions
+#' @description
 #'   \code{get_frequent_activities()} retrieves a list of a user's frequent activities.
 #'
 #' @inheritParams inheritparams_token
@@ -191,94 +159,98 @@ get_activity_type <- function(token, activity_id)
 #'   See \url{https://dev.fitbit.com/reference/web-api/activity/#get-frequent-activities}  for more details.
 #'
 #' @export
-get_frequent_activities <- function(token)
-{
-  url <- paste0(url_activity, "frequent.json")
-  tidy_output(get(url, token), TRUE)
-}
+get_frequent_activities <- make_get_function(paste0(url_activity, "frequent.json"))
 
-#' Get Recent Activity Types
+#' @title Get Recent Activity Types
 #'
-#' Retrieves a list of a user's recent activities types logged with some details of the last activity log of that type.
+#' @description
+#'   \code{get_recent_activity_types()} retrieves a list of a user's recent activities types logged with some details of the last activity log of that type.
 #'
 #' @inheritParams inheritparams_token
 #' @inheritParams inheritparams_simplify
 #'
-#' @export
-get_recent_activity_types <- function(token)
-{
-  url <- paste0(url_activity, "recent.json")
-  tidy_output(get(url, token), TRUE)
-}
-
-#' Get Favorite Activities
+#' @details
+#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#get-recent-activity-types} for more details.
 #'
-#' Returns a list of a user's favorite activities.
+#' @export
+get_recent_activity_types <- make_get_function(paste0(url_activity, "recent.json"))
+
+#' @title Get Favorite Activities
+#'
+#' @description
+#'   \code{get_favorite_activities()} returns a list of a user's favorite activities.
 #'
 #' @inheritParams inheritparams_token
+#' @inheritParams inheritparams_simplify
+#'
+#' @details
+#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#get-favorite-activities} for more details.
 #'
 #' @export
-get_favorite_activities <- function(token)
-{
-  url <- paste0(url_activity, "favorite.json")
-  tidy_output(get(url, token), TRUE)
-}
+get_favorite_activities <- make_get_function(paste0(url_activity, "favorite.json"))
 
-#' Add Favorite Activity
+#' @title Add Favorite Activity
 #'
-#' The Add Favorite Activity endpoint adds the activity with the given ID to user's list of favorite activities.
-#' Note: A successful request returns a 201 response code with an empty body.
+#' @description
+#'   \code{add_favorite_activity()} adds the activity with the given ID to user's list of favorite activities.
 #'
 #' @param token An OAuth 2.0 token generated by oauth_token()
 #' @param activity_id	The ID of the activity to add to user's favorites.
+#'
+#' @details
+#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#add-favorite-activity} for more details.
+#'
 #' @export
 add_favorite_activity <- function(token, activity_id)
 {
   #POST https://api.fitbit.com/1/user/-/activities/favorite/[activity-id].json
   url <- paste0(url_activity, sprintf("favorite/%s.json", activity_id))
-  response <- post(url, token, list())
-  convert_content_to_r_object(response)
+  invisible(post(url, token, body=NULL))
 }
 
-#' Delete Favorite Activity
+#' @title Delete Favorite Activity
 #'
-#' The Delete Favorite Activity removes the activity with the given ID (activity_id) from a user's list of favorite activities.
+#' @description
+#'   \code{delete_favorite_activity()} removes the activity with the given ID (activity_id) from a user's list of favorite activities.
 #'
-#' @param token An OAuth 2.0 token generated by oauth_token()
-#' @param activity_id	The ID of the activity to be removed.
+#' @inheritParams inheritparams_token
+#' @param activity_id The ID of the activity to be removed.
+#'
+#' @details
+#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#delete-favorite-activity} for more details.
+#'
 #' @export
 delete_favorite_activity <- function(token, activity_id)
 {
   url <- paste0(url_activity, sprintf("favorite/%s.json", activity_id))
-  response <- delete(url, token)
-  convert_content_to_r_object(response)
+  invisible(delete(url, token))
 }
 
-#' Get Activity Goals
+#' @title Get Activity Goals
 #'
-#' The Get Activity Goals retrieves a user's current daily or weekly activity goals using measurement units as defined in the unit system, which corresponds to the Accept-Language header provided.
-#' Considerations
-#' Only the current goals are returned.
-#' Floors goal only returned for users who currently or have previously paired a Fitbit device with an altimeter.
-#' Calories out goal represents either dynamic daily target from the Premium trainer plan or manual calorie burn goal.
-#' The default daily goal values are:
-#' 10 floors
-#' 30 active minutes
-#' Calories out is user specific, which accounts for BMR
+#' @description
+#'   \code{get_activity_goals()} retrieves a user's current daily or weekly activity goals
 #'
-#' @param token An OAuth 2.0 token generated by oauth_token()
-#' @param period daily or weekly
+#' @inheritParams inheritparams_token
+#' @param period "daily" or "weekly"
+#' @inheritParams inheritparams_simplify
+#'
+#' @details
+#' See \url{https://dev.fitbit.com/reference/web-api/activity/#get-activity-goals} for more details.
+#'
 #' @export
-get_activity_goals <- function(token, period)
+get_activity_goals <- function(token, period, simplify=TRUE)
 {
+  stop_if_x_is_not_in(period, c("daily", "weekly"))
+
   url <- paste0(url_activity, sprintf("goals/%s.json", period))
-  response <- get(url, token)
-  convert_content_to_r_object(response)
+  tidy_output(get(url, token), simplify)
 }
 
-#' Update Activity Goals
+#' @title Update Activity Goals
 #'
-#' The Update Activity Goals endpoint creates or updates a user's daily activity goals and returns a response using units in the unit system which corresponds to the Accept-Language header provided.
+#' @description
+#'   \code{update_activity_goals()} creates or updates a user's daily activity goals.
 #'
 #' @inheritParams inheritparams_token
 #' @param period daily or weekly
@@ -288,29 +260,32 @@ get_activity_goals <- function(token, period)
 #' @param distance optional	Goal value; in the format X.XX or integer.
 #' @param steps optional	Goal value; integer.
 #'
+#' @details
+#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#update-activity-goals} for more details.
+#'
 #' @export
-update_activity_goals <- function(token, period, calories_out, active_minutes, floors, distance, steps)
+update_activity_goals <- function(token, period, calories_out=NULL, active_minutes=NULL, floors=NULL, distance=NULL, steps=NULL)
 {
   #POST https://api.fitbit.com/1/user/[user-id]/activities/goals/[period].json
   url <- paste0(url_activity, sprintf("goals/%s.json", period))
-  response <- post(url, token)
-  convert_content_to_r_object(response)
+  post_arguments <- c("calories_out", "active_minutes", "floors", "distance", "steps")
+  body <- purrr::map(rlang::set_names(post_arguments), ~ rlang::eval_tidy(rlang::sym(.x)))
+  names(body) <- stringr::str_replace_all(names(body), "(_)([a-z])", function(x){stringr::str_to_upper(stringr::str_sub(x, 2))})
+  tidy_output(post(url, token, body), simplify=TRUE)
 }
 
-#' Get Lifetime Stats
+#' @title Get Lifetime Stats
 #'
-#' Retrieves the user's activity statistics in the format requested using units in the unit system which corresponds to the Accept-Language header provided. Activity statistics includes Lifetime and Best achievement values from the My Achievements tile on the website dashboard. Response contains both statistics from the tracker device and total numbers including tracker data and manual activity log entries as seen on the Fitbit website dashboard.
-#'
-#' Privacy Setting
-#' The My Achievements (Friends or Anyone) privacy permission grants access to user's resource.
+#' @description
+#'   \code{get_lifetime_stats()} retrieves the user's activity statistics.
+#'   Activity statistics includes Lifetime and Best achievement values from the My Achievements tile on the website dashboard.
+#'   Response contains both statistics from the tracker device and total numbers including tracker data and manual activity log entries as seen on the Fitbit website dashboard.
 #'
 #' @inheritParams inheritparams_token
+#' @inheritParams inheritparams_simplify
+#'
+#' @details
+#' See \url{https://dev.fitbit.com/reference/web-api/activity/#get-lifetime-stats} for more details.
 #'
 #' @export
-get_lifetime_stats <- function(token)
-{
-  #GET https://api.fitbit.com/1/user/[user-id]/activities.json
-  url <- paste0(url_api, "activities.json")
-  response <- get(url, token)
-  convert_content_to_r_object(response)
-}
+get_lifetime_stats <- make_get_function(paste0(url_api, "activities.json"))
