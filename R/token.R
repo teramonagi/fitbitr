180f00e23be4f4b8c665eb9d73a5bb0dacf7b429
Nagi Teramo <teramonagi@gmail.com>
2018-01-04 16:59:26 +0900

    Add language and locale support

--- R/token.R
+++ R/token.R
@@ -4,18 +4,57 @@
 #'   Generate an oauth token for fitbit.
 #'   To get key and secret, You have to register your own information on https://dev.fitbit.com/jp.
 #'
-#' @param key client key
-#' @param secret secret key
-#' @param callback callback URL configured by your own
+#' @param key Client key
+#' @param secret Secret key
+#' @param callback Callback URL configured by your own
+#' @param locale Locales which is suitable for displaying a translated response if available.
+#'   Possible values: c(en_AU | fr_FR | de_DE | ja_JP | en_NZ | es_ES | en_GB | en_US)
+#' @param language The unit systems of values.
+#'   Possible values: c(en_US | en_GB | other )
+#' @details
+#'   See \url{https://dev.fitbit.com/reference/web-api/basics/#language} for more details.
 #'
 #' @export
-oauth_token <- function(key=NULL, secret=NULL, callback=NULL)
+oauth_token <- function(key=NULL, secret=NULL, callback=NULL, locale=NULL, language=NULL)
+{
+  #Load key automatically from global or environmnt variable
+  keys <- tidy_key_and_secret(key, secret)
+
+  #Load redirect URI from global or environment variable
+  callback <- tidy_callback(callback)
+
+  # We need to create header, see the following links in details.
+  # https://community.fitbit.com/t5/Web-API-Development/Invalid-authorization-header-format/td-p/1363901
+  # https://community.fitbit.com/t5/Web-API-Development/Trouble-with-OAuth-2-0-Tutorial/m-p/1617571#M6583
+  header <- httr::add_headers(Authorization=paste0("Basic ", RCurl::base64Encode(charToRaw(paste0(keys$key, ":", keys$secret)))))
+  content_type <- httr::content_type("application/x-www-form-urlencoded")
+
+  # What types of data do we allow to access -> as possible as we can
+  scope <- c("sleep", "activity", "heartrate", "location", "nutrition", "profile", "settings", "social", "weight")
+
+  endpoint <- create_endpoint()
+  myapp <- httr::oauth_app("r-package", key=keys$key, secret=keys$secret, redirect_uri=callback)
+  list(
+    token=httr::oauth2.0_token(endpoint, myapp, scope=scope, config_init=c(header, content_type), cache=FALSE),
+    locale=locale,
+    language=language
+  )
+}
+
+create_endpoint <- function()
+{
+  request <- "https://api.fitbit.com/oauth2/token"
+  authorize <- "https://www.fitbit.com/oauth2/authorize"
+  access <- "https://api.fitbit.com/oauth2/token"
+  httr::oauth_endpoint(request, authorize, access)
+}
+
+tidy_key_and_secret <- function(key, secret)
 {
   if((is.null(key) && !is.null(secret)) || (!is.null(key) && is.null(secret))){
     stop("Both of key and secret must be given.")
   }
 
-  #Load key automatically from global or environmnt variable
   if(is.null(key) && is.null(secret)){
     if(Sys.getenv("FITBIT_KEY") != "" && Sys.getenv("FITBIT_SECRET") != ""){
       # Found in environment variables
@@ -29,34 +68,20 @@ oauth_token <- function(key=NULL, secret=NULL, callback=NULL)
       stop("You must specify your key and secret.")
     }
   }
-
-  #Load redirect URI from global or environment variable
-  if(is.null(callback)){
-    if(exists("FITBIT_CALLBACK")){
-      callback <- FITBIT_CALLBACK
-    } else{
-      callback <- "http://localhost:1410/"
-    }
-  }
-
-  # We need to create header, see the following links in details.
-  # https://community.fitbit.com/t5/Web-API-Development/Invalid-authorization-header-format/td-p/1363901
-  # https://community.fitbit.com/t5/Web-API-Development/Trouble-with-OAuth-2-0-Tutorial/m-p/1617571#M6583
-  header <- httr::add_headers(Authorization=paste0("Basic ", RCurl::base64Encode(charToRaw(paste0(key, ":", secret)))))
-  content_type <- httr::content_type("application/x-www-form-urlencoded")
-
-  # What types of data do we allow to access -> as possible as we can
-  scope <- c("sleep", "activity", "heartrate", "location", "nutrition", "profile", "settings", "social", "weight")
-
-  endpoint <- create_endpoint()
-  myapp <- httr::oauth_app("r-package", key=key, secret=secret, redirect_uri=callback)
-  httr::oauth2.0_token(endpoint, myapp, scope=scope, config_init=c(header, content_type), cache=FALSE)
+  list(key=key, secret=secret)
 }
 
-create_endpoint <- function()
+tidy_callback <- function(callback)
 {
-  request <- "https://api.fitbit.com/oauth2/token"
-  authorize <- "https://www.fitbit.com/oauth2/authorize"
-  access <- "https://api.fitbit.com/oauth2/token"
-  httr::oauth_endpoint(request, authorize, access)
+  if(is.null(callback)){
+    if(Sys.getenv("FITBIT_CALLBACK") != ""){
+      Sys.getenv("FITBIT_CALLBACK")
+    } else if(exists("FITBIT_CALLBACK")){
+      FITBIT_CALLBACK
+    } else{
+      "http://localhost:1410/"
+    }
+  } else{
+    callback
+  }
 }
